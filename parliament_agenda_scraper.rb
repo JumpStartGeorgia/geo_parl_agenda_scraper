ce2507a2f2098319fe92ad02a6202d5d03510f8c
ericnbarrett <ericnbarrett@gmail.com>
2014-03-15 15:40:26 +0400

    refactored into loop

--- parliament_agenda_scraper.rb
+++ parliament_agenda_scraper.rb
@@ -29,21 +29,22 @@ def month_to_int(mstring)
 end
 
 url = ARGV[0] || "http://parliament.ge/index.php?option=com_content&view=category&layout=blog&id=59&Itemid=520&lang=ge"
-
 page = Nokogiri::HTML(open(url).read)
-
 days = page.css('table.blog > tr')
-string_day = days[0].css("td.contentheading").text.chomp
-
-day_string = string_day.scan(/\d+/).first
-month_string = string_day.scan(/\D+/).join.gsub(' ', '').strip
-year_string = Time.now.year
-time_string = days[0].css('table')[1].css('strong')[0].text[0,5]
-hour_string, minute_string = time_string.split(':')
-
-date = DateTime.new(year_string.to_i, month_to_int(month_string), day_string.to_i, hour_string.to_i, minute_string.to_i, 0, '+3')
-
 
 days.each do |day|
+
+  # get date
+
+  
+  string_day = day.css("td.contentheading").text.strip
+  day_string = string_day.scan(/\d+/).first
+  month_string = string_day.scan(/\D+/).join.gsub(' ', '').strip
+  year_string = Time.now.year
+  time_string = days[0].css('table')[1].css('strong')[0].text[0,5]
+  hour_string, minute_string = time_string.split(':')
+  date = DateTime.new(year_string.to_i, month_to_int(month_string), day_string.to_i, hour_string.to_i, minute_string.to_i, 0, '+3')
+  
+  #
   
 end
